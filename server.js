49ff29fae5852de92d833ad2fa9412013254a1b0
ankitmhn <ankit.m@hurestic.com>
2020-04-27 06:28:01 +0530

    updated SSL config

--- server.js
+++ server.js
@@ -1,41 +1,48 @@
 #!/usr/bin/env node
- 
-const GuacamoleLite = require('guacamole-lite');
-const express = require('express');
-const http = require('http');
-const cors = require('cors');
+
+const GuacamoleLite = require("guacamole-lite");
+const express = require("express");
+const https = require("https");
+const fs = require("fs");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 
 const PORT = 8081;
-const server = http.createServer(app);
- 
+const server = https.createServer(
+  {
+    key: fs.readFileSync("./privkey.pem"),
+    cert: fs.readFileSync("./cert.pem"),
+  },
+  app
+);
+
 const websocketOptions = {
-    port: PORT // we will accept connections to this port
+  port: PORT, // we will accept connections to this port
 };
- 
+
 const guacdOptions = {
-    port: 4822, // port of guacd
-    host: "127.0.0.1"
+  port: 4822, // port of guacd
+  host: "127.0.0.1",
 };
- 
+
 const clientOptions = {
-    crypt: {
-        cypher: 'AES-256-CBC',
-        key: 'MySuperSecretKeyForParamsToken12'
+  crypt: {
+    cypher: "AES-256-CBC",
+    key: "MySuperSecretKeyForParamsToken12",
+  },
+  log: {
+    level: "DEBUG",
+    stdLog: (...args) => {
+      console.log("[DEBUG]", ...args);
     },
-    log: {
-        level: 'DEBUG',
-        stdLog: (...args) => {
-            console.log('[DEBUG]', ...args)
-        },
-        errorLog: (...args) => {
-            console.error('[ERROR]', ...args)
-        }
-    }
+    errorLog: (...args) => {
+      console.error("[ERROR]", ...args);
+    },
+  },
 };
- 
-const guacServer = new GuacamoleLite(websocketOptions, guacdOptions, clientOptions);
-//const guacServer = new GuacamoleLite({server}, guacdOptions, clientOptions);
-//server.listen(PORT);
+
+// const guacServer = new GuacamoleLite(websocketOptions, guacdOptions, clientOptions);
+const guacServer = new GuacamoleLite({ server }, guacdOptions, clientOptions);
+server.listen(PORT);
